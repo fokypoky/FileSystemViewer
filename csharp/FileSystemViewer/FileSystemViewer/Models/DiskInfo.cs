3758acfbe57fc45af56d2b1596a09394ef07f352
Anton Petukhov <petuhova805@gmail.com>
2023-09-26 11:41:47 +0300

    Тип файловой системы

--- csharp/FileSystemViewer/FileSystemViewer/Models/DiskInfo.cs
+++ csharp/FileSystemViewer/FileSystemViewer/Models/DiskInfo.cs
@@ -3,13 +3,15 @@
     public class DiskInfo
     {
 	    public string DiskName { get; set; }
+        public string FileSystem { get; set; }
         public decimal Volume { get; set; }
         public decimal FreeSpace { get; set; }
         public decimal UsedSpace { get; set; }
 
-        public DiskInfo(string diskName, decimal volume, decimal freeSpace, decimal usedSpace)
+        public DiskInfo(string diskName, string fileSystem , decimal volume, decimal freeSpace, decimal usedSpace)
         {
 	        DiskName = diskName;
+            FileSystem = fileSystem;
 	        Volume = volume;
 	        FreeSpace = freeSpace;
 	        UsedSpace = usedSpace;
