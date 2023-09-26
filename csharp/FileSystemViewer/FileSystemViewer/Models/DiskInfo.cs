0f2ef7e9e82c2e3f477f5aa32f53393121e0ef70
Anton Petukhov <petuhova805@gmail.com>
2023-09-26 12:26:39 +0300

    Переименовал свойство общего размера

--- csharp/FileSystemViewer/FileSystemViewer/Models/DiskInfo.cs
+++ csharp/FileSystemViewer/FileSystemViewer/Models/DiskInfo.cs
@@ -4,15 +4,15 @@
     {
 	    public string DiskName { get; set; }
         public string FileSystem { get; set; }
-        public decimal Volume { get; set; }
+        public decimal TotalSize { get; set; }
         public decimal FreeSpace { get; set; }
         public decimal UsedSpace { get; set; }
 
-        public DiskInfo(string diskName, string fileSystem , decimal volume, decimal freeSpace, decimal usedSpace)
+        public DiskInfo(string diskName, string fileSystem , decimal totalSize, decimal freeSpace, decimal usedSpace)
         {
 	        DiskName = diskName;
             FileSystem = fileSystem;
-	        Volume = volume;
+	        TotalSize = totalSize;
 	        FreeSpace = freeSpace;
 	        UsedSpace = usedSpace;
         }
