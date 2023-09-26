6275acd67722bee058824d1cd7170619d3e719a4
Anton Petukhov <petuhova805@gmail.com>
2023-09-26 11:31:07 +0300

    Модель информации о диске

---
+++ csharp/FileSystemViewer/FileSystemViewer/Models/DiskInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace FileSystemViewer.Models
+{
+    public class DiskInfo
+    {
+	    public string DiskName { get; set; }
+        public decimal Volume { get; set; }
+        public decimal FreeSpace { get; set; }
+        public decimal UsedSpace { get; set; }
+
+        public DiskInfo(string diskName, decimal volume, decimal freeSpace, decimal usedSpace)
+        {
+	        DiskName = diskName;
+	        Volume = volume;
+	        FreeSpace = freeSpace;
+	        UsedSpace = usedSpace;
+        }
+    }
+}
